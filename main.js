6ba0e0fbd5483e98433bffb902c4925ee93361c0
Cristian Garcia <crisg@sas.upenn.edu>
2023-11-13 13:26:21 -0500

    created a function that displays coins and their prices to a coins section; added a variable that limits how many coin articles one can add to the page; added a copy of the coin articles to improve te banner infinite scroll. display 5 new articles at a time when every form submit

--- main.js
+++ main.js
@@ -1,19 +1,56 @@
-let walletCount = 3;
+let coinCount = 0;
+let articleCount = 3;
+const maxArticles = 13;
 
-const createArticle = () => {
+const coins = ["BTC", "ETH", "USDT", "BNB", "XRP", "SOL", "USDC", "ADA", "DOGE", "TRX"];
+coinsStr = coins.join(",");
+
+const createArticle = (tokenName, tokenPrice, tokenSymbol) => {
     const article = document.createElement("article");
     const h2 = document.createElement("h2");
-    h2.textContent = `Wallet ${++walletCount}`;
+    const h3 = document.createElement("h3");
+    h2.textContent = `${tokenName}`;
+    h3.textContent = `${tokenPrice}${tokenSymbol}`;
     article.append(h2);
+    article.append(h3);
 
     return article;
+}
+
+const displayTokenAndPrice = (tokenObjects, numOfTokens) => {
+    const keys = Object.keys(tokenObjects);
+    for (let i = coinCount; i < coinCount + numOfTokens; i++) {
+        if (i >= keys.length || articleCount >= maxArticles) {
+            break;
+        }
+        const tokenName = keys[i];
+        const article = createArticle(tokenName, tokenObjects[tokenName]["USD"], "USD");
+        articleCount++;
+        
+        // append to both wallet sections
+        const walletsSection = document.querySelector(".wallets .primary");
+        walletsSection.append(article);
+
+        const walletsSection2 = document.querySelector(".wallets .secondary");
+        const article2 = createArticle(tokenName, tokenObjects[tokenName]["USD"], "USD");
+        walletsSection2.append(article2);
+    }
+    coinCount += numOfTokens;
 }
 
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const article = createArticle();
-    const walletsSection = document.querySelector("section.wallets");
-    walletsSection.append(article);
+    fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinsStr}&tsyms=USD`, {
+        headers: {
+            "authorization": {API_KEY2}
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        displayTokenAndPrice(data, 5);
+    })
+    .catch((error) => console.log(error));
 })
